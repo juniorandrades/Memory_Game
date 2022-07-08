1142f1bf9de198dbd3b41d51739b72b7acb95331
juniorandrades <juniorandrades2000@gmail.com>
2022-07-08 17:52:07 -0300

    Display grid adicionado + JS
    O game já ganhou o formato e a funcionalidade de embaralhar e virar, porém falta as regras e algumas dinâmicas.

---
+++ assets/js/script.js
@@ -0,0 +1,129 @@
+const FRONT = "card-front";
+const BACK = "card-back";
+const CARD = "card";
+const ICON = "icon";
+
+let techs = ['bootstrap',
+ 'css',
+ 'electron',
+ 'firebase',
+ 'html',
+ 'javascript',
+ 'jquery',
+ 'mongo',
+ 'node',
+ 'react'];
+
+
+ let cards = null;
+
+ startGame();
+
+ function startGame () {
+    //criando os modelos visual das cartas
+    cards = createCardsFromTechs(techs);
+    shuffleCards(cards);
+
+    initializeCards(cards);
+ }
+
+ function initializeCards (cards){
+    let gameBoard = document.getElementById("gameBoard");
+
+    cards.forEach (card => {
+
+        let cardElement = document.createElement('div');
+        cardElement.id = card.id; // add id
+        cardElement.classList.add(CARD); // add class
+        cardElement.dataset.icon = card.icon // add icon
+
+        createCardContent (card, cardElement);
+
+        cardElement.addEventListener('click', flipCard);
+        gameBoard.appendChild(cardElement);
+
+    })
+
+ }
+
+
+ function createCardContent (card, cardElement) {
+
+        createCardFace(FRONT, card, cardElement);
+        createCardFace(BACK, card, cardElement)
+
+ }
+
+function createCardFace (face, card, element) {
+
+    let cardElementFace = document.createElement('div');
+    cardElementFace.classList.add(face);
+    if (face === FRONT) {
+
+        let iconElement = document.createElement('img');
+        iconElement.classList.add(ICON);
+        iconElement.src = "./assets/images/" + card.icon + ".png"
+        cardElementFace.appendChild(iconElement);
+
+    }else{
+        cardElementFace.innerHTML = "&lt/&gt";
+    }
+    element.appendChild(cardElementFace);
+}
+
+ function shuffleCards (cards) {
+    let currentIndex = cards.length;
+    let randomIndex = 0;
+
+    while (currentIndex !== 0) {
+
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        [cards[randomIndex], cards[currentIndex]] = [cards[currentIndex], cards[randomIndex]]
+
+    }
+
+ }
+
+
+function createCardsFromTechs (techs) {
+
+    let cards = [];
+
+    techs.forEach((tech) =>  {
+        cards.push(createPairFromTech(tech));
+    })
+   return cards.flatMap(pair => pair);
+}
+
+function createPairFromTech (tech) {
+
+    return [{   
+        id: createIdWithTech(tech),
+        icon: tech,
+        flipped: false,
+
+    },  {         
+        id: createIdWithTech(tech),
+        icon: tech,
+        flipped: false,
+    
+    }]
+
+
+}
+
+function createIdWithTech (tech) {
+    return tech + parseInt(Math.random() * 1000);
+}
+
+function flipCard () {
+
+    this.classList.add('flip')
+}
+
+
+
+
+
