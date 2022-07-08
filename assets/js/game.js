94a3495452c9949039e2b4d3320faa32962a40cd
juniorandrades <juniorandrades2000@gmail.com>
2022-07-08 20:59:00 -0300

    Bugs da jogabilidade foram corrigidos

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,5 +1,54 @@
 let game = {
 
+    lockMode: false,
+    firstCard: null,
+    secondCard: null,
+
+    setCard: function (id) {
+
+     let card =  this.cards.filter (card => card.id === id) [0];
+
+     if (card.flipped || this.lockMode) {
+        return false;
+     }
+
+     if (!this.firstCard){
+        this.firstCard = card;
+        this.firstCard.flipped = true;
+        return true;
+     }else {
+        this.secondCard = card;
+        this.secondCard.flipped = true;
+        this.lockMode = true;
+        return true;
+     }
+
+    },
+
+
+    checkMatch: function () {
+        //verificar se a carta 2 está selecionada.
+        if (!this.firstCard || !this.secondCard) {
+          return false;
+        }
+        return this.firstCard.icon === this.secondCard.icon;
+      },
+    
+
+
+    clearCards: function () {
+        this.firstCard = null;
+        this.secondCard = null;
+        this.lockMode = false;
+    },
+
+    unflipCards: function () {
+        this.firstCard.flipped = false;
+        this.secondCard.flipped = false;
+        this.clearCards();
+      },
+
+
     techs: ['bootstrap',
     'css',
     'electron',
--- assets/js/script.js
+++ assets/js/script.js
@@ -80,17 +80,47 @@ function createPairFromTech (tech) {
     
     }]
 
-
 }
 
 function createIdWithTech (tech) {
     return tech + parseInt(Math.random() * 1000);
 }
 
-function flipCard () {
-
-    this.classList.add('flip')
-}
+//flip cards
+function flipCard() {
+    if (game.setCard(this.id)) {
+      this.classList.add("flip");
+  
+      //validando se temos duas cartas selecionadas.
+      if (game.secondCard) {
+        /**chamando a função para verificar se são iguais.
+         * colocamos um if quando o match for true, executar outra função
+         * de limpar as variáveis.
+         */
+        if (game.checkMatch()) {
+          game.clearCards();
+          //verificando se é deu fim de jogo
+          if (game.checkGameOver()) {
+            let gameOverlayer = document.getElementById("gameOver");
+            gameOverlayer.style.display = "flex";
+          }
+        } else {
+          /** caso não, pegamos os id das cartas que estão flipada e
+           * removemos a class flip
+           */
+  
+          setTimeout(() => {
+            let firstCardView = document.getElementById(game.firstCard.id);
+            let secondCardView = document.getElementById(game.secondCard.id);
+  
+            firstCardView.classList.remove("flip");
+            secondCardView.classList.remove("flip");
+            game.unflipCards();
+          }, 1000);
+        }
+      }
+    }
+  }
 
 
 
